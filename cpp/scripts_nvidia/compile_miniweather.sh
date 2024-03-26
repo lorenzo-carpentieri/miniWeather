9463df4899584ba114956c3230a331016d1a419b
lorenzo-carpentieri <lorenzocarpentieri34@gmail.com>
2024-03-26 15:28:14 +0000

    Added scripts for NVIDIA A30

---
+++ cpp/scripts_nvidia/compile_miniweather.sh
@@ -0,0 +1,21 @@
+APPROACH=$1
+HIDING=$2
+cmake \
+  -DCMAKE_CXX_COMPILER="/opt/intel/oneapi/compiler/2024.0/bin/icpx" \
+  -DCMAKE_CXX_FLAGS="-lmpi -ffast-math -Wno-deprecated -fsycl -fsycl-targets=nvptx64-nvidia-cuda -Xsycl-target-backend --cuda-gpu-arch=sm_80 -O3" \
+  -DSYNERGY_SYCL_IMPL="DPC++" \
+  -DSYNERGY_KERNEL_PROFILING=OFF \
+  -DSYNERGY_DEVICE_PROFILING=ON \
+  -DSYNERGY_HOST_PROFILING=OFF \
+  -DSYNERGY_USE_PROFILING_ENERGY=ON \
+  -DSYNERGY_CUDA_SUPPORT=ON \
+  -DSYNERGY_CUDA_ARCH=sm_80 \
+  -DYAKL_ARCH=SYCL \
+  -DYAKL_SYCL_FLAGS="-fsycl -fsycl-targets=nvptx64-nvidia-cuda -Xsycl-target-backend --cuda-gpu-arch=sm_80 -O3" \
+  -DNX=128 \
+  -DNZ=64 \
+  -DFREQ_SCALING_APPROACH="$APPROACH" \
+  -DMPI_HIDING=$HIDING \
+  -DSIM_TIME=10 \
+  -DLDFLAGS="-lpnetcdf" \
+  ..
